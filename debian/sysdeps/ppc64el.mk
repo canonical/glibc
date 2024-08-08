1ebe9a7e93f83de10ca5e0db953b2aeb5fec4b76
Simon Chopin <schopin@ubuntu.com>
2024-08-08 16:47:42 +0200

    2.39-0ubuntu8.3 (patches unapplied)
    Imported using git-ubuntu import.

--- debian/sysdeps/ppc64el.mk
+++ debian/sysdeps/ppc64el.mk
@@ -5,4 +5,4 @@ extra_config_options = --enable-multi-arch --with-cpu=power8
 libc_rtlddir = /lib64
 # loop unrolling causes false positive warnings
 # see: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=92955
-libc_extra_cflags = -O3 -fno-tree-vectorize -Wno-error=stringop-overflow -fno-omit-frame-pointer
+libc_extra_cflags = -O3 -fno-tree-vectorize -Wno-error=stringop-overflow
